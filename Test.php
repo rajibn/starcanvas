2ad07e49c22440dd18cc537aa45fe40fe41b805d
rajibn <36919954+rajibn@users.noreply.github.com>
2018-03-06 21:08:42 +0530

    Update Test.php

--- Test.php
+++ Test.php
@@ -7,9 +7,9 @@
 <body>
 
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 define("USERNAME", "rajibnaskar@codaemonsoftwares.com");
 define("PASSWORD", "Kolkata2018");
 define("SECURITY_TOKEN", "TQoyPnDyU3uBjFjiVVdS4ULjO"); //3MVG9d8..z.hDcPLRQ5Bwzc1G2fOHLOTGFq3OqayThBeUTH24SF5FWDFcSfMEaYojoLBbMjnFTHT_ZyWUBxWt
@@ -22,7 +22,7 @@ $mySforceConnection->login(USERNAME, PASSWORD.SECURITY_TOKEN);
 
 echo $query = "SELECT Id, FirstName, LastName, Phone from Contact";
 $response = $mySforceConnection->query($query);
-echo $query1 = "Update Contact set Phone="03321219999" where Id='0037F00000RT4oZQAT'";
+echo $query1 = "Update Contact set Phone="03321219999" where Id="0037F00000RT4oZQAT"";
 $response2 = $mySforceConnection->query($query1);
 
 ?>
