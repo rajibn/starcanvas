74ccc1b1b94b26029de3bced0b1d386cc629497e
rajibn <36919954+rajibn@users.noreply.github.com>
2018-03-06 23:19:08 +0530

    update Update.php

--- Update.php
+++ Update.php
@@ -14,7 +14,8 @@ define("USERNAME", "rajibnaskar@codaemonsoftwares.com");
 define("PASSWORD", "Kolkata2018");
 define("SECURITY_TOKEN", "TQoyPnDyU3uBjFjiVVdS4ULjO"); //3MVG9d8..z.hDcPLRQ5Bwzc1G2fOHLOTGFq3OqayThBeUTH24SF5FWDFcSfMEaYojoLBbMjnFTHT_ZyWUBxWt
 
-require_once ('soapclient/SforcePartnerClient.php');	
+require_once ('soapclient/SforcePartnerClient.php');
+	echo 'hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh';
 $mySforceConnection = new SforcePartnerClient();
 $mySforceConnection->createConnection("PartnerWSDL.xml");
 $mySforceConnection->login(USERNAME, PASSWORD.SECURITY_TOKEN);
